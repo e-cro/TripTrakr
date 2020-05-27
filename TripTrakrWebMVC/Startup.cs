068959b6d009b26c268b308d716fa0d913064d3c
Erin Croteau <erin.croteau@gmail.com>
2020-05-27 07:46:14 -0400

    Initial commit.

---
+++ TripTrakrWebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TripTrakrWebMVC.Startup))]
+namespace TripTrakrWebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
